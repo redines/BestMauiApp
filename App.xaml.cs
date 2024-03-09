2f422f407e058070debe2304bfbddaa1b52dfc47
Redines <pontus.goth@outlook.com>
2024-03-09 16:33:27 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BestMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BestMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
